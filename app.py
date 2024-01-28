5878ea79a340602ed78e27fabaad90da02e5d0df
Pragyan Jyoti Dutta <74003758+cyber-prags@users.noreply.github.com>
2024-01-28 01:33:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -470,5 +470,5 @@ with app.app_context():
     db.create_all()
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000)
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0",port=5000)
